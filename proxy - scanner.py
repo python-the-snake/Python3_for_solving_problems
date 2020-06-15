9900b76ec612b20334d88383f2756bd5323f4b7b
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-06-15 18:49:48 +0300

    proxy

---
+++ proxy - scanner.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+def crawl_proxies():
+    proxies=[]
+    link = 'https://www.sslproxies.org/'
+
+    r = requests.det(link)
+    s = BeautifulSoup(r.text, 'html.parser')
+    for i in s.find_all('tr')[:30]:
+        try:
+            data = i.find_all('td')
+            adress = data[0].text
+            port = data[1].text
+            type_ = data[4].text
+            proxy = address + ':' + port
+            proxies.append({'https':proxy})
+
+        except:
+            pass
+
+        return proxies
+
+    proxies = crawl_proxies()
+    pront(proxies)
